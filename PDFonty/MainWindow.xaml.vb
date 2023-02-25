43b577ada967417fd409da4ca3fb138477c476f6
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2023-02-25 19:22:35 +1000

    统计重复字体能力

--- PDFonty/MainWindow.xaml.vb
+++ PDFonty/MainWindow.xaml.vb
@@ -3,6 +3,7 @@ Imports System.Environment
 Imports Microsoft.Win32
 Imports System.Windows.Forms
 Imports iText.Kernel.Pdf
+Imports Microsoft.VisualBasic.Logging
 
 Class MainWindow
     Dim CurrentFile As String = ""
@@ -32,4 +33,39 @@ Class MainWindow
         Status.Content = "共计" + Pages.ToString + "页"
         Return Nothing
     End Function
+
+    Private Sub AntiAcrobat_Click(sender As Object, e As RoutedEventArgs) Handles AntiAcrobat.Click
+        Status.Content = "正在读取字体列表"
+        Dim FontStatistic As Dictionary(Of String, Integer) = New Dictionary(Of String, Integer)
+        Dim OriginFontStatistic As Dictionary(Of String, Integer) = New Dictionary(Of String, Integer)
+        Dim ObjectNum = PDFDocument.GetNumberOfPdfObjects
+        Dim CurrentObjectNum As Integer = 1
+        While CurrentObjectNum <= ObjectNum
+            Dim CurrentObject = PDFDocument.GetPdfObject(CurrentObjectNum)
+            If CurrentObject IsNot Nothing AndAlso CurrentObject.GetObjectType() = 3 Then
+                Dim Dict As PdfDictionary = CurrentObject
+                If Dict.GetAsName(PdfName.Type) Is PdfName.Font Then
+                    '得到原始的字体名称
+                    Dim FontName = Dict.GetAsName(PdfName.BaseFont).ToString()
+                    '格式化为标准字体名称
+                    FontName = FontName.Substring(FontName.IndexOf("+") + 1)
+                    '统计去除前12字符后的字体名
+                    Dim OriginFontName = FontName.Substring(12)
+                    If Not FontStatistic.ContainsKey(FontName) Then
+                        FontStatistic(FontName) = 1
+                        If Not OriginFontStatistic.ContainsKey(OriginFontName) Then
+                            OriginFontStatistic(OriginFontName) = 1
+                        Else
+                            OriginFontStatistic(OriginFontName) += 1
+                        End If
+                    Else
+                            FontStatistic(FontName) += 1
+                    End If
+                End If
+            End If
+            CurrentObjectNum += 1
+        End While
+        Status.Content = "统计完成，开始施工"
+        Status.Content = "处理完成"
+    End Sub
 End Class
