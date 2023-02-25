40be8d91f30e50703bad659e9f3231bb7ae29d87
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2023-02-25 15:00:10 +1000

    简单写个UI

--- PDFonty/MainWindow.xaml.vb
+++ PDFonty/MainWindow.xaml.vb
@@ -1,3 +1,24 @@
-﻿Class MainWindow
+﻿Imports iText
+Imports System.Environment
+Imports Microsoft.Win32
+Imports System.Windows.Forms
+
+Class MainWindow
+    Dim CurrentFile = ""
+    Private Sub Open_Click(sender As Object, e As RoutedEventArgs) Handles Open.Click
+        Dim fd As New Windows.Forms.OpenFileDialog() With {
+                                .CheckFileExists = True,
+                                .CheckPathExists = True,
+                                .Title = "选择一个PDF文件",
+                                .Multiselect = False,
+                                .RestoreDirectory = True,
+                                .InitialDirectory = CurrentDirectory
+                                }
+        If fd.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            CurrentFile = fd.FileName
+            FileName.Content = CurrentFile
+        Else
+        End If
+    End Sub
 
 End Class
