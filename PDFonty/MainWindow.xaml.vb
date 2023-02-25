0e97c6f9696a17a8fb589ef42d4bd792f5507865
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2023-02-25 20:14:19 +1000

    支持可变长度

--- PDFonty/MainWindow.xaml.vb
+++ PDFonty/MainWindow.xaml.vb
@@ -4,6 +4,7 @@ Imports Microsoft.Win32
 Imports System.Windows.Forms
 Imports iText.Kernel.Pdf
 Imports Microsoft.VisualBasic.Logging
+Imports System.Text.RegularExpressions
 
 Class MainWindow
     Dim CurrentFile As String = ""
@@ -35,6 +36,14 @@ Class MainWindow
     End Function
 
     Private Sub AntiAcrobat_Click(sender As Object, e As RoutedEventArgs) Handles AntiAcrobat.Click
+        Dim result As DialogResult = MessageBox.Show("你确定这个PDF需要字体去重吗？若查看PDF字体列表所有字体名称前都有6或6的整数倍个随机字符，才需要执行该操作。对不需要该操作的PDF执行可能会导致PDF显示异常。", "PDFonty", MessageBoxButtons.YesNo)
+        If Not result = System.Windows.Forms.DialogResult.Yes Then
+            Return
+        End If
+        If CurrentFile = "" Then
+            Status.Content = "未选择文件"
+            Return
+        End If
         Status.Content = "正在读取字体列表"
         Dim FontStatistic As Dictionary(Of String, Integer) = New Dictionary(Of String, Integer)
         Dim OriginFontStatistic As Dictionary(Of String, Integer) = New Dictionary(Of String, Integer)
@@ -49,23 +58,37 @@ Class MainWindow
                     Dim FontName = Dict.GetAsName(PdfName.BaseFont).ToString()
                     '格式化为标准字体名称
                     FontName = FontName.Substring(FontName.IndexOf("+") + 1)
-                    '统计去除前12字符后的字体名
-                    Dim OriginFontName = FontName.Substring(12)
-                    If Not FontStatistic.ContainsKey(FontName) Then
-                        FontStatistic(FontName) = 1
-                        If Not OriginFontStatistic.ContainsKey(OriginFontName) Then
-                            OriginFontStatistic(OriginFontName) = 1
+                    '统计去除前字符后的字体名
+                    '一定有傻逼往这个输入框里填不是整数的东西，所以做一个检测，去酒吧点炒饭的有一个算一个都是傻逼
+                    If Not New Regex("^[0-9]*$").IsMatch(AcroFontLength.Text) Then
+                        Status.Content = "你在只允许数字的输入框里输了什么寄吧玩意"
+                        Return
+                    End If
+                    Dim SubLength = Int32.Parse(AcroFontLength.Text)
+                    If FontName.Length > SubLength Then
+                        Dim OriginFontName = FontName.Substring(SubLength)
+                        If Not FontStatistic.ContainsKey(FontName) Then
+                            FontStatistic(FontName) = 1
+                            If Not OriginFontStatistic.ContainsKey(OriginFontName) Then
+                                OriginFontStatistic(OriginFontName) = 1
+                            Else
+                                OriginFontStatistic(OriginFontName) += 1
+                            End If
                         Else
-                            OriginFontStatistic(OriginFontName) += 1
-                        End If
-                    Else
                             FontStatistic(FontName) += 1
+                        End If
                     End If
                 End If
             End If
             CurrentObjectNum += 1
         End While
         Status.Content = "统计完成，开始施工"
+        Dim ToReplaceFonts
+        For Each Font In OriginFontStatistic
+            If Font.Value > 1 Then
+
+            End If
+        Next
         Status.Content = "处理完成"
     End Sub
 End Class
